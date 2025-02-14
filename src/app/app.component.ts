9a39fa1e4861e4cdba288c0b1d3faa752aeae1c8
Cristian Lopez <cristian.c.lopez.m@gmail.com>
2025-02-14 16:33:41 -0500

    creating my first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+/**
+ * @selector app-root is root-component of the application
+ * @templateUrl is the html file of the component
+ * @styleUrls is the css file of the component (aplies to the component only)
+ * @standalone is a boolean property which is used to define whether the component is standalone or not.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
